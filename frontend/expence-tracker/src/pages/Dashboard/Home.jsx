e207bb7b0599d992b7b59b1e8add2ab9a1c661ac
Jeeswan Bajracharya <csit22081033_jeeswan@achsnepal.edu.np>
2026-09-06 15:52:54 +0545

    Frontend: Sidemenu completed

--- frontend/expence-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expence-tracker/src/pages/Dashboard/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import DashboardLayout from "../../components/layouts/DashboardLayout";
 
 const Home = () => {
   return (
-    <div>
-      h
-    </div>
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="my-5 mx-auto"></div>
+    </DashboardLayout>
   )
 }
 
